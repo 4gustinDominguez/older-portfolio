5063903bdce380f5e21e32cc601002520aeb3210
frangaliana <fjgalianacano@gmail.com>
2019-09-24 12:27:03 +0200

    master: modified initial configuration (3)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import * as React from "react";
-import { render } from "react-dom";
-import "./styles.css";
+import * as React from 'react';
+import { render } from 'react-dom';
+import './styles.css';
 
 type AppProps = {};
 
-const App: React.FunctionComponent<AppProps> = () => (
-  <>{`Hi, I'm fg93 website!`}</>
-);
+const App: React.FunctionComponent<AppProps> = () => <>{`Hi, I'm fg93 website!`}</>;
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const basePath = __dirname;
 
-module.exports = (mode = "development") => ({
-  entry: path.join(basePath, "src", "index.tsx"),
+module.exports = (mode = 'development') => ({
+  entry: path.join(basePath, 'src', 'index.tsx'),
   output: {
-    path: path.join(basePath, "dist"),
-    filename: "bundle.js"
+    path: path.join(basePath, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
   },
-  devtool: mode === "development" ? "inline-source-map" : "",
+  devtool: mode === 'development' ? 'inline-source-map' : '',
   devServer: {
     port: 3000,
-    contentBase: path.join(basePath, "dist"),
+    contentBase: path.join(basePath, 'dist'),
     open: true,
     compress: true,
     watchContentBase: true,
-    progress: true
+    progress: true,
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
-              modules: true
-            }
-          }
-        ]
+              modules: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(basePath, "src", "index.html"),
-      filename: "index.html"
-    })
-  ]
+      template: path.join(basePath, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
 });
