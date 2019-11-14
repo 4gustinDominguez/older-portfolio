31a01de723998db99326094578ed30265203b818
frangaliana <fjgalianacano@gmail.com>
2019-11-14 16:15:53 +0100

    master: improved bundle doing code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,22 @@ module.exports = (mode = 'development') => ({
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(basePath, 'src', 'index.html'),
