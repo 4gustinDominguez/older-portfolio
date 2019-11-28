9b6fe9d647d705eb238654a9836bb45cb47c54b6
frangaliana <fjgalianacano@gmail.com>
2019-11-28 19:56:07 +0100

    i18n-translations: commented saved theme behaviour in localStorage

--- src/app.hooks.ts
+++ src/app.hooks.ts
@@ -5,10 +5,11 @@ export const useThemeMode = (): { mode: Mode; toggleMode: () => void } => {
   const [mode, setMode] = React.useState<Mode>('light');
 
   const setTheme = (theme: Mode) => {
-    window.localStorage.setItem('mode', theme);
+    // window.localStorage.setItem('mode', theme);
     setMode(theme);
   };
 
+  /*
   React.useEffect(() => {
     const localMode = window.localStorage.getItem('mode') as Mode;
 
@@ -18,6 +19,7 @@ export const useThemeMode = (): { mode: Mode; toggleMode: () => void } => {
       setTheme('light');
     }
   }, []);
+  */
 
   const toggleMode = () => {
     if (mode === 'light') {
