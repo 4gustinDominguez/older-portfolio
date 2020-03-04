04570d59d34296fc3493055284941e67727697eb
frangaliana <fjgalianacano@gmail.com>
2020-03-04 16:57:14 +0100

    master: modified styles between lines

--- src/common/styled-fonts/title.styled.ts
+++ src/common/styled-fonts/title.styled.ts
@@ -4,6 +4,7 @@ export const Title = styled.h1`
   font-size: ${({ theme: { spacing } }) => spacing(8)}px;
   color: ${({ theme: { palette } }) => palette.primary};
   font-weight: 700;
+  line-height: ${({ theme: { spacing } }) => spacing(7)}px;
   margin: 0;
   padding: 0;
 `;
