8ba4b06ba55f28f66eb4a888e59dd91a10233569
frangaliana <fjgalianacano@gmail.com>
2020-03-06 11:57:15 +0100

    master: deleted split in chunks

--- webpack.config.js
+++ webpack.config.js
@@ -68,20 +68,6 @@ module.exports = (mode = 'development') => ({
   },
   optimization: {
     minimizer: [new UglifyJsPlugin()],
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
   },
   plugins: [
     new HtmlWebpackPlugin({
